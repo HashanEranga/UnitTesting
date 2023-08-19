292c11cb7b53b1c31b870442da4a83c7c167e85f
HashanEPerera <45037796+HashanEranga@users.noreply.github.com>
2023-08-19 20:54:02 +0530

    UT - Tested cases that throws an exception

--- UnitTestProject.UnitTests/ErrorLogger.Tests/ErrorLogger.Tests.cs
+++ UnitTestProject.UnitTests/ErrorLogger.Tests/ErrorLogger.Tests.cs
@@ -17,4 +17,14 @@ public class ErrorLogger_Tests
         
         Assert.That(_errorLogger.LastError, Is.EqualTo("sample error"));
     }
+
+    [Test]
+    [TestCase(null)]
+    [TestCase("")]
+    [TestCase(" ")]
+    public void Log_InvalidLogger_ThrowArgumentNullException(string error)
+    {
+        Assert.That(()=> _errorLogger.Log(error), Throws.ArgumentNullException);
+        Assert.That(()=> _errorLogger.Log(error), Throws.Exception.TypeOf<ArgumentNullException>());
+    }
 }
